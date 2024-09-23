97af276fd10aee6e0a134e5343234f13c79b119e
FikalAhmad <fikaalm26@gmail.com>
2024-09-23 22:12:26 +0700

    refactor: change structure folder

--- src/_components/Sidebar/SidebarBase.tsx
+++ src/_components/Sidebar/SidebarBase.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
-import kopikanLogo from "@/app/assets/logo/logo-cup-hijau.png";
-import newOrder from "@/app/assets/images/newOrder.svg";
+import kopikanLogo from "@/public/assets/logo/logo-cup-hijau.png";
+import newOrder from "@/public/assets/images/newOrder.svg";
+import dashboard from "@/public/assets/images/dashboard.svg";
+import onlineOrder from "@/public/assets/images/onlineOrders.svg";
+import settings from "@/public/assets/images/settings.svg";
+import logout from "@/public/assets/images/logout.svg";
 
 const SidebarBase = () => {
   const sidebarItem = [
@@ -9,32 +13,32 @@ const SidebarBase = () => {
       name: "New Order",
     },
     {
-      image: newOrder,
+      image: dashboard,
       name: "Dashboard",
     },
     {
-      image: newOrder,
+      image: onlineOrder,
       name: "Online Order",
     },
     {
-      image: newOrder,
+      image: settings,
       name: "Settings",
     },
     {
-      image: newOrder,
+      image: logout,
       name: "Logout",
     },
   ];
 
   return (
-    <div className="w-[130px] flex flex-col gap-3">
+    <div className="w-[130px] h-screen pt-5 flex flex-col gap-3 items-center border-2">
       <Image src={kopikanLogo} alt="Kopikan Logo" width={152} height={85} />
       <div className="flex flex-col gap-6">
         {sidebarItem.map((item, index) => {
           return (
             <div
               key={index + 1}
-              className="flex flex-col justify-center items-center w-20 h-20 p-[10px] bg-white rounded"
+              className="flex flex-col justify-center items-center w-20 h-20 p-[10px] bg-white gap-[10px] rounded"
             >
               <Image src={item.image} alt="New Order" />
               <div className="text-[12px] text-center">{item.name}</div>

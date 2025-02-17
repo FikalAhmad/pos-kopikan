fa7b6a8e129ed22fbfd6e314bbb223c0d71bda20
FikalAhmad <fikaalm26@gmail.com>
2025-02-17 14:15:59 +0700

    add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // const authToken = request.cookies.get("refreshToken");
-  // console.log(authToken);
-  // const publicPaths = ["/login", "/register"];
-  // const { nextUrl } = request;
-  // // Check if the path is public
-  // const isPublicPath = publicPaths.includes(nextUrl.pathname);
-  // // Redirect to login if accessing protected route without token
-  // if (!authToken && !isPublicPath) {
-  //   return NextResponse.redirect(new URL("/login", nextUrl));
-  // }
-  // if (authToken && isPublicPath) {
-  //   return NextResponse.redirect(new URL("/dashboard", nextUrl));
-  // }
-  // return;
+  const authToken = request.cookies.get("refreshToken");
+  console.log(authToken);
+  const publicPaths = ["/login", "/register"];
+  const { nextUrl } = request;
+  // Check if the path is public
+  const isPublicPath = publicPaths.includes(nextUrl.pathname);
+  // Redirect to login if accessing protected route without token
+  if (!authToken && !isPublicPath) {
+    return NextResponse.redirect(new URL("/login", nextUrl));
+  }
+  if (authToken && isPublicPath) {
+    return NextResponse.redirect(new URL("/dashboard", nextUrl));
+  }
+  return;
 }
 
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};

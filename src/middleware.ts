f8e3f5e5e17aafcbbe7562534b02d1e245f79a33
FikalAhmad <fikaalm26@gmail.com>
2025-08-22 21:14:55 +0700

    fix: token cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const authToken = request.cookies.get("refreshToken");
+  const authToken = request.cookies.get("token");
   const publicPaths = ["/login", "/register"];
   const path = request.nextUrl.pathname;
 
